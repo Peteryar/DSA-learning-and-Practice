885faf1e813e20d80b05ed25396cabb264707916
JSVenger <peteryar19@gmail.com>
2022-01-26 01:10:37 +0100

    insertionSort solution

---
+++ sort_insertionSort.js
@@ -0,0 +1,31 @@
+//write a function that takes an array and sort the items from smallest to largest using insertion sort algorithm
+
+//pseudo
+//-- loop through each item in the array using i starting from 0 and less than arr.length
+//--  start a nested loop from i+1 to 0
+// compare item at j to j-1 and insert it in it's possible position
+
+function insertionSort(arr){
+    for(let i = 0; i<arr.length; i++){
+        for(let j=i+1; j>0; j--){
+            if(!arr[j]){
+                break
+            }
+            if(arr[j] < arr[j-1]){
+                [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+            }
+             console.log(arr)
+        }
+        console.log('outer---------->')
+    }
+
+    return arr
+}
+
+
+
+
+
+
+
+insertionSort([1, 2, 3, 4, 5])
