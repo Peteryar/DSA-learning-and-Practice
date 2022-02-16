ce4b03641e2da89318cee42df39b8fff0ff43763
JSVenger <peteryar19@gmail.com>
2022-02-16 21:50:46 +0100

    Optimized es6 insertion sort algorithm

--- sort_insertionSort.js
+++ sort_insertionSort.js
@@ -1,23 +1,34 @@
 //write a function that takes an array and sort the items from smallest to largest using insertion sort algorithm
 
+// The idea behind sorting by insertion is to take each element in the array and 
+// insert them in their actual position
+
 //pseudo
 //-- loop through each item in the array using i starting from 0 and less than arr.length
-//--  start a nested loop from i+1 to 0
-// compare item at j to j-1 and insert it in it's possible position
+// create a variable call currentEl to get the value of the current Element using arr[i]
+//--  start a nested loop using j starting from i-1 to 0
+// -- the loop only runs and decrease if currentEl < arr[j]
 
-function insertionSort(arr){
-   for(let i = 1; i<arr.length; i++){
-       var currentEl = arr[i]
+// -- if the loop runs, insert the element in arr[j] in index j+1, this is because
+// -- if the loop runs, arr[j] > currentEl
 
-       for(var j = i-1; j>= 0 && arr[j] > currentEl; j-- ){
-          arr[j+1] = arr[j]
-          console.log('j', j)
-       }
-      console.log('arr', arr)
-       arr[j] = currentEl
-   } 
+// At the bottom of the outer loop, set the currentEl to the element in the inner loop last index loop successfully run
+
+// return arr
+
+function insertionSort(arr) {
+   for (let i = 1; i < arr.length; i++) {
+
+      var currentEl = arr[i]
+      let trackIndex = i;
+      for (let j = i - 1; j >= 0 && arr[j] > currentEl; j--) {
+         trackIndex = j
+         arr[j + 1] = arr[j]
+      }
+       arr[trackIndex] = currentEl
+   }
 
    return arr
 }
 
-console.log(insertionSort([2, 5, 3, 7]))
+console.log(insertionSort([2, 5, 1, 0, 12, 15, 19, 7, 4, 13]))
