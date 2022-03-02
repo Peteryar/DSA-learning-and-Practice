73f01e91f1818fc6797cd22d6ae5c95a1862c0c4
JSVenger <peteryar19@gmail.com>
2022-03-02 15:23:29 +0100

    mergeTwo arrays bug fix for universal application

--- MergeTwoSortedArray/mergeTwoSortedArray.js
+++ MergeTwoSortedArray/mergeTwoSortedArray.js
@@ -1,18 +1,33 @@
-function mergeTwoSortedArrays(arr1, arr2){
+function mergeSortedArray (arr1, arr2){
+     
      let newArr = [];
+
      let i = 0;
      let j = 0;
-
-     while(arr1[i]||arr2[j]){
-          if(arr1[i] < arr2[j]){
+     
+     while(arr1[i] || arr2[j]){
+          console.log(i, j)
+          console.log('arr1', arr1[i])
+          console.log('arr2', arr2[j])
+          
+          if(!arr1[i]){   
+             newArr.push(arr2[j])
+               j++
+          }
+          else if(!arr2[j]){
+               newArr.push(arr1[i])
+               i++
+          }else if(arr1[i] < arr2[j]){
                newArr.push(arr1[i])
                i++
           }else{
                newArr.push(arr2[j])
                j++
           }
-     }
-     return newArr
+          console.log('newArr', newArr)
+      }
+   
+    return newArr
 }
 
-console.log(mergeTwoSortedArrays([2, 4, 8, 20, 25], [3, 6, 7, 12, 13, 18, 21, 24, 27]))
+mergeSortedArray([5, 12, 15, 17, 19, 20, 21], [2])
