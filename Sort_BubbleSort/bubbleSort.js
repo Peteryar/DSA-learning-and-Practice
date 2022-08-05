12c204ad21b3e1e587691cbbaeae2bc68149486a
JSVenger <peteryar19@gmail.com>
2022-08-05 18:44:30 +0100

    fix selection sort error

--- Sort_BubbleSort/bubbleSort.js
+++ Sort_BubbleSort/bubbleSort.js
@@ -1,18 +1,21 @@
 function bubbleSort(arr) {
-    for (let i = arr.length; i >= 0; i--) {
+    for (let i = arr.length; i > 0; i--) {
         let noSwap = true;
         for (let j = 1; j < i; j++) {
-            if (arr[j] < arr[j-1]) {
+            if (arr[j] < arr[j - 1]) {
                 noSwap = false;
-                [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
             }
         }
-        if(noSwap){
-            break
+        if (noSwap) {
+            break;
         }
+        console.log('arr', arr)
     }
     return arr
 }
-const data = Array.apply(null, { length: 100000 }).map(a => Math.floor(Math.random() * 100000))
-console.log('bubbleSortData===>', data)
-console.log(bubbleSort(data))
+
+console.log(bubbleSort([
+    0, 1, 3, 6, 7,
+    4, 5, 8, 2, 9
+  ]))
--- Sort_SelectionSort/selectionSort.js
+++ Sort_SelectionSort/selectionSort.js
@@ -3,7 +3,7 @@ function selectionSort(arr){
         let smallIndex = i;
         for(let j=i+1; j<arr.length; j++){
             if(arr[smallIndex] > arr[j]){
-                index = j
+                smallIndex = j
             }
         }
         [arr[i], arr[smallIndex]] = [arr[smallIndex], arr[i]];
@@ -12,6 +12,6 @@ function selectionSort(arr){
 }
 
 const data = Array.apply(null, {length:100000}).map(a=>Math.floor(Math.random()*1000000))
-console.log('mergeSortData===>', data)
+// console.log('mergeSortData===>', data)
 
 console.log(selectionSort(data));
